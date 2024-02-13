6979c96c0a4b469c3a4bb3d0c71dce2dac771fd2
emdad <emdad@ergo-ventures.com>
2024-02-13 15:07:30 +0600

    multimodule - changed Dependencies.kt file commented out redundant dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,15 +2,15 @@ object Versions {
     const val core = "1.12.0"
     const val lifecycle = "2.7.0"
     const val compose = "1.8.2"
-    const val compose_bom = "1.8.2"
-    const val material = "1.11.0"
     const val appcompat = "1.6.1"
     const val constraintlayout = "2.1.4"
     const val navigationfragment = "2.7.6"
     const val junit = "4.13.2"
     const val junit_ext = "1.1.5"
+    const val material = "1.11.0"
     const val espresso_core="3.5.1"
-    const val android_test_bom = "2023.08.00"
+//    const val compose_bom = "1.8.2"
+//    const val android_test_bom = "2023.08.00"
 }
 //implementation("androidx.core:core-ktx:1.12.0")
 //androidx.lifecycle:lifecycle-runtime-ktx
@@ -29,11 +29,11 @@ object Deps {
 
 object Compose {
     const val compose = "androidx.activity:activity-compose:${Versions.compose}"
-    const val compose_bom = "androidx.compose:compose-bom:${Versions.compose_bom}"
-    const val compose_ui = "androidx.compose.ui:ui"
-    const val compose_graphics = "androidx.compose.ui:ui-graphics"
-    const val compose_ui_tooling = "androidx.compose.ui:ui-tooling-preview"
-    const val compose_material = "androidx.compose.material3:material3"
+//    const val compose_bom = "androidx.compose:compose-bom:${Versions.compose_bom}"
+//    const val compose_ui = "androidx.compose.ui:ui"
+//    const val compose_graphics = "androidx.compose.ui:ui-graphics"
+//    const val compose_ui_tooling = "androidx.compose.ui:ui-tooling-preview"
+//    const val compose_material = "androidx.compose.material3:material3"
 }
 
 object Navigation {
@@ -44,17 +44,17 @@ object Navigation {
 
 object TestDeps {
     const val junit = "junit:junit:${Versions.junit}"
-    const val compose_junit = "androidx.compose.ui:ui-test-junit4"
+//    const val compose_junit = "androidx.compose.ui:ui-test-junit4"
 }
 
 object AndroidTestDeps{
     const val junit_ext = "androidx.test.ext:junit:${Versions.junit_ext}"
     const val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso_core}"
-    const val compose_bom = "androidx.compose:compose-bom:${Versions.android_test_bom}"
+//    const val compose_bom = "androidx.compose:compose-bom:${Versions.android_test_bom}"
 
 }
 
 object DebugDeps{
-    const val ui_tooling = "androidx.compose.ui:ui-tooling"
-    const val ui_test_manifest = "androidx.compose.ui:ui-test-manifest"
+//    const val ui_tooling = "androidx.compose.ui:ui-tooling"
+//    const val ui_test_manifest = "androidx.compose.ui:ui-test-manifest"
 }
